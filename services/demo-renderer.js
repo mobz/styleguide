68d8c70b4f9d47744575d05fcfdae27566a52ab0
Li Juen Chang <ljchang@aconex.com>
2016-09-21 17:27:49 +1000

    Generate demo tabs dynamically from .md file contents

--- services/demo-renderer.js
+++ services/demo-renderer.js
@@ -1,17 +1,30 @@
+const cheerio = require( 'cheerio' );
 const fs = require( 'fs' );
 const handlebars = require( 'handlebars' );
 const path = require( 'path' );
 
-const template = fs.readFileSync( 'views/demo-iframe.handlebars', 'utf8' );
+const TEMPLATE = fs.readFileSync( 'views/demo-iframe.handlebars', 'utf8' );
+const SECTION_ATTR = 'styleguide-content';
+const SECTION_SELECTOR = `[${SECTION_ATTR}]`;
 
 module.exports = {
 	getDemoContainer: ( demoFilePath ) => {
 		let iframeName = path.basename( demoFilePath );
 		let iframeSrc = `/demo?file=${encodeURIComponent( demoFilePath )}`;
-		let code = fs.readFileSync( demoFilePath, 'utf8' );
-		let vm = { iframeName, iframeSrc, code };
 
-		return handlebars.compile( template )( vm );
+		let code = fs.readFileSync( demoFilePath, 'utf8' );
+		let $ = cheerio.load( code );
+
+		let tabs = [];
+
+		$( SECTION_SELECTOR ).map( ( idx, item ) => {
+			let id = $( item ).attr( SECTION_ATTR );
+			let contents = $( item ).html();
+			tabs.push( { id, contents } );
+		} );
+
+		let vm = { iframeName, iframeSrc, tabs };
+		return handlebars.compile( TEMPLATE )( vm );
 	},
 	getIframeContents: ( demoFilePath ) => {
 		let contents = fs.readFileSync( demoFilePath, 'utf8' );
